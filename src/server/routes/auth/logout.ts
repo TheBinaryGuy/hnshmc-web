1323f38da2c2f8e880c5a65268d07dc331c5f864
TheBinaryGuy <hello@thebinary.dev>
2024-09-22 21:49:32 +0530

    added admin logout

--- src/server/routes/auth/logout.ts
+++ src/server/routes/auth/logout.ts
@@ -1,26 +1,59 @@
 import type { ContextVariables } from '@/server/types';
-import { lucia } from '@/services/auth';
+import { lucia, luciaAdmin } from '@/services/auth';
 import { createRoute, OpenAPIHono } from '@hono/zod-openapi';
 
-export const logout = new OpenAPIHono<{ Variables: ContextVariables }>().openapi(
-    createRoute({
-        method: 'get',
-        path: '/api/auth/logout',
-        tags: ['Auth'],
-        summary: 'Logout',
-        responses: {
-            200: {
-                description: 'Success',
+export const logout = new OpenAPIHono<{ Variables: ContextVariables }>()
+    .openapi(
+        createRoute({
+            method: 'get',
+            path: '/api/auth/logout',
+            tags: ['Auth'],
+            summary: 'Logout',
+            responses: {
+                200: {
+                    description: 'Success',
+                },
             },
-        },
-    }),
-    async c => {
-        const session = c.get('session');
-        if (!session) {
+        }),
+        async c => {
+            const authHeader = c.req.header('Authorization');
+            if (!authHeader) {
+                return c.json({});
+            }
+
+            const sessionId = lucia.readBearerToken(authHeader);
+            if (!sessionId) {
+                return c.json({});
+            }
+
+            await lucia.invalidateSession(sessionId);
             return c.json({});
         }
+    )
+    .openapi(
+        createRoute({
+            method: 'get',
+            path: '/api/auth/admin-logout',
+            tags: ['Auth'],
+            summary: 'Admin Logout',
+            responses: {
+                200: {
+                    description: 'Success',
+                },
+            },
+        }),
+        async c => {
+            const authHeader = c.req.header('AdminAuthorization');
+            if (!authHeader) {
+                return c.json({});
+            }
 
-        await lucia.invalidateSession(session.id);
-        return c.json({});
-    }
-);
+            const sessionId = luciaAdmin.readBearerToken(authHeader);
+            if (!sessionId) {
+                return c.json({});
+            }
+
+            await luciaAdmin.invalidateSession(sessionId);
+            return c.json({});
+        }
+    );
